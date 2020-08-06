43c861930c768e586fb260255b87a2faa3faccaf
astanusic <adrienpro.stanusic@gmail.com>
2020-08-06 15:38:16 +0200

    feat: react-router-dom setup + pages and components created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import "./App.css";
+import HomePage from "./Pages/HomePage/HomePage";
+import AboutPage from "./Pages/AboutPage/AboutPage";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Portfolio</h1>
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route path="/about">
+          <AboutPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/Pages/HomePage/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./HomePage.css";
+import Navigation from "../../Components/Navigation/Navigation";
+
+function HomePage() {
+  return (
+    <section className="homepage">
+      <Navigation />
+    </section>
+  );
+}
+
+export default HomePage;
